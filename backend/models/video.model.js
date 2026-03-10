5d89bfd8f10d50bac1b8f9b975cd19b5c7adef52
Rupeshkumar663 <rupeshsamrat08@gmail.com>
2026-03-10 18:52:05 +0530

    Updated project files

---
+++ backend/models/video.model.js
@@ -0,0 +1,38 @@
+import mongoose, { Schema } from "mongoose";
+const videoSchema=mongoose.Schema({
+        videoFile:{
+            type:String,//cloudinary and Multer
+            required:true,
+        },
+        thumbnail:{
+            type:String,//cloudinary and Multer
+            required:true,
+        },
+        title:{
+             type:String,
+            required:true,
+        },
+        description:{
+            type:String,
+            required:true,
+        },
+        duration:{
+            type:Number,
+            required:true,
+        },
+        views:{
+            type:Number,
+            default:0,
+        },
+        isPublished:{
+            type:Boolean,
+            default:false,
+        },
+        owner:{
+            type:Schema.Types.ObjectId,
+            ref:"User",
+        }
+    
+},{TimeStamps:true})
+export const Video=mongoose.model("Video",videoSchema)
+
